9ad7e73139b2fb5a37f6c305ccdb4d6e2ad13634
dibot55 <diebots555@gmail.com>
2023-05-09 17:12:38 -0600

    unknown type

---
+++ src/02-unknown-type.ts
@@ -0,0 +1,38 @@
+// El remplazo de los Any. Unknown types - Permite asignarle a una variable diferentes tipos
+
+// ANY
+let anyVar:any = 12321;
+anyVar = "Hola";
+anyVar = true;
+anyVar = {nombre:"Pepe"};
+anyVar = () => {
+  return "";
+};
+
+let isNew: boolean = anyVar; // ??????
+console.log(isNew);
+
+anyVar.toLowerCase();// ??????? NO USAR ANY
+
+// Unknown - Permite la flexibilidad de un ANY sin apagar el analisis de codigo estatico
+let unknownVar: unknown;
+
+unknownVar = true;
+unknownVar = undefined;
+unknownVar = {nombre:""};
+unknownVar = 1;
+
+// let isNew2: boolean = unknownVar // Verifica el tipo
+// unknownVar.toUpperCase(); // TS te indica que le indiques un tipo primero.
+
+if (typeof unknownVar === "string") {
+  unknownVar.toUpperCase();
+}; // Asi si permite que le asignemos un metodo de un tipo en concreto
+
+// Unknown en funciones
+
+const parse = (str: string):unknown => {
+  return JSON.parse(str);
+};
+
+JSON.parse('{name: "dibot55"}');
